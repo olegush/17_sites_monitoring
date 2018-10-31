bb7f2ba98c15044dfc530dd2620c154553700df6
olegush <43958767+olegush@users.noreply.github.com>
2018-10-31 19:09:07 +0300

    added check connection, improved the code

--- check_sites_health.py
+++ check_sites_health.py
@@ -5,12 +5,13 @@ import requests
 import whois
 
 
-def parser_args():
+def get_args_parser():
     parser = argparse.ArgumentParser(description='Sites Monitoring Utility')
     parser.add_argument('filepath', help='path to file with urls')
     parser.add_argument('--days',
                         help='days to expire, 30 by default',
-                        type=int)
+                        type=int,
+                        default=30)
     args = parser.parse_args()
     return args
 
@@ -21,31 +22,32 @@ def load_strings_urls(path):
 
 
 def is_server_respond_ok(url):
-    response = requests.get(url)
-    return response.ok
+    try:
+        response = requests.get(url)
+        return response.ok
+    except requests.ConnectionError:
+        return False
 
 
 def get_domain_is_expire(domain, days_to_expire):
     whois_data = whois.whois(domain)
-    if not whois_data['expiration_date']:
-        return '-'
-    elif isinstance(whois_data['expiration_date'], list):
+    if isinstance(whois_data['expiration_date'], list):
         date_expire = whois_data['expiration_date'][0]
-    else:
+    elif whois_data['expiration_date']:
         date_expire = whois_data['expiration_date']
+    else:
+        return 'No data'
     return (date_expire - datetime.now()).days < days_to_expire
 
 
 if __name__ == '__main__':
-    whois_args = parser_args()
+    whois_args = get_args_parser()
     days_to_expire = whois_args.days
     filepath = whois_args.filepath
     if not filepath:
         exit('No script parameter (path to txt file)')
     if not os.path.isfile(filepath):
         exit('No such file')
-    if not days_to_expire:
-        days_to_expire = 30
     urls = load_strings_urls(filepath)
     print('\nServer responds (is respond with status OK) and \ndomains '
           'expiration (is domain expires less than {} days:\n'
@@ -53,5 +55,8 @@ if __name__ == '__main__':
     print('{:<25}{:^10}{:^10}'.format('SITE', 'STATUS', 'EXPIRE'))
     for url in urls:
         is_ok = is_server_respond_ok(url)
-        is_expire = get_domain_is_expire(url, days_to_expire)
+        if is_ok:
+            is_expire = get_domain_is_expire(url, days_to_expire)
+        else:
+            is_expire = 'Error'
         print('{:<25}{:^10}{:^10}'.format(url, is_ok, is_expire))
